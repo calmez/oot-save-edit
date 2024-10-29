97f3fc1a694ee01512fcf8772adf454dab8f692e
Conrad Calmez <conrad@calmez.net>
2024-10-29 21:32:14 +0100

    structured code a bit more

--- main_test.ts
+++
@@ -1,5 +0,0 @@
-import { assert } from "@std/assert";
-
-Deno.test(function dummy() {
-  assert(1 == 1);
-});
---
+++ savefile_test.ts
@@ -0,0 +1,18 @@
+import { assertInstanceOf } from "@std/assert";
+import { SaveFile, SaveHeader, SaveSlot } from "./savefile.ts";
+
+Deno.test(function shouldCreate() {
+  const instance = new SaveFile();
+  assertInstanceOf(instance, SaveFile);
+});
+
+Deno.test(function shouldInitilizeEmpty() {
+  const instance = new SaveFile();
+  assertInstanceOf(instance.header, SaveHeader);
+  instance.slots.forEach(slot => {
+    assertInstanceOf(slot, SaveSlot);
+  });
+  instance.backups.forEach(slot => {
+    assertInstanceOf(slot, SaveSlot);
+  });
+});
