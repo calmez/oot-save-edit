a2ccbaff2f5b4bd9f6ca51f6087bc6f019a0e148
Conrad Calmez <conrad@calmez.net>
2024-10-31 20:43:48 +0100

    implemented more saveheader functionality

--- models/saveheader.test.ts
+++ models/saveheader.test.ts
@@ -1,30 +1,72 @@
 import { assertEquals, assertInstanceOf, assertThrows } from "@std/assert";
 import { SaveHeader } from "./saveheader.ts";
-import { SoundOption } from "./SaveHeader.ts";
+import { LanguageOption, SoundOption, ZTargetOption } from "./SaveHeader.ts";
 
 Deno.test({
-  name: 'should create',
+  name: "should create",
   fn() {
     const instance = new SaveHeader();
     assertInstanceOf(instance, SaveHeader);
-  }
+  },
 });
 
 Deno.test({
-  name: 'should raise an error when incorrect size of data is given',
+  name: "should raise an error when incorrect size of data is given",
   fn() {
     const wrongInput = new Uint8Array(42);
     assertThrows(() => new SaveHeader(wrongInput));
-  }
+  },
 });
 
 Deno.test({
-  name: 'should provide the stored SoundOption',
+  name: "should provide the stored SoundOption",
   fn() {
     const testData = new Uint8Array(SaveHeader.requiredSize);
     const expectedSoundOption = SoundOption.Headphones;
     testData[0x00] = expectedSoundOption;
     const instance = new SaveHeader(testData);
     assertEquals(instance.soundOption, expectedSoundOption);
-  }
+  },
+});
+
+Deno.test({
+  name: "should provide the stored ZTargetOption",
+  fn() {
+    const testData = new Uint8Array(SaveHeader.requiredSize);
+    const expectedZTargetOption = ZTargetOption.Hold;
+    testData[0x01] = expectedZTargetOption;
+    const instance = new SaveHeader(testData);
+    assertEquals(instance.zTargetOption, expectedZTargetOption);
+  },
+});
+
+Deno.test({
+  name: "should provide the stored LanguageOption",
+  fn() {
+    const testData = new Uint8Array(SaveHeader.requiredSize);
+    const expectedLanguageOption = LanguageOption.German;
+    testData[0x02] = expectedLanguageOption;
+    const instance = new SaveHeader(testData);
+    assertEquals(instance.languageOption, expectedLanguageOption);
+  },
+});
+
+Deno.test({
+  name: "should check when data is not valid",
+  fn() {
+    const invalidData = new Uint8Array(SaveHeader.requiredSize);
+    const instance = new SaveHeader(invalidData);
+    assertEquals(instance.isValid, false);
+  },
+});
+
+Deno.test({
+  name: "should check when data is valid",
+  fn() {
+    const validData = new Uint8Array(SaveHeader.requiredSize);
+    validData.set(SaveHeader.validCheckPattern, 0x03);
+    validData.set(SaveHeader.defaultPadding, 0x0C);
+    const instance = new SaveHeader(validData);
+    assertEquals(instance.isValid, true);
+  },
 });
--- models/saveheader.ts
+++ models/saveheader.ts
@@ -5,12 +5,41 @@ export enum SoundOption {
   Surround,
 }
 
+export enum ZTargetOption {
+  Switch = 0x00,
+  Hold = 0x01,
+  Invalid,
+}
+
+export enum LanguageOption {
+  English = 0x00,
+  German = 0x01,
+  French = 0x02,
+}
+
 export class SaveHeader {
   static get requiredSize(): number {
     return 0x20;
   }
 
-  constructor(private bytes: Uint8Array = new Uint8Array(SaveHeader.requiredSize)) {
+  static get validCheckPattern(): Uint8Array {
+    const encoder = new TextEncoder();
+    return new Uint8Array([0x98, 0x09, 0x10, 0x21, ...encoder.encode("ZELDA")]);
+  }
+
+  static get defaultPadding(): Uint8Array {
+    return new Uint8Array(5);
+  }
+
+  static get debugPadding(): Uint8Array {
+    let data = new Uint8Array(5);
+    data.fill(0xAB);
+    return data;
+  }
+
+  constructor(
+    private bytes: Uint8Array = new Uint8Array(SaveHeader.requiredSize),
+  ) {
     if (bytes.length != SaveHeader.requiredSize) {
       throw Error(
         `Save header needs to be of length ${SaveHeader.requiredSize}, got ${bytes.length}`,
@@ -20,5 +49,41 @@ export class SaveHeader {
 
   get soundOption(): SoundOption {
     return this.bytes[0x00];
+  }
+
+  get zTargetOption(): ZTargetOption {
+    return this.bytes[0x01];
+  }
+
+  get languageOption(): LanguageOption {
+    return this.bytes[0x02];
+  }
+
+  private get checkPattern(): Uint8Array {
+    return this.bytes.slice(0x03, 0x03 + 9);
+  }
+
+  private get padding(): Uint8Array {
+    return this.bytes.slice(0x0C, 0x0C + 5);
+  }
+
+  private get isPatternValid(): boolean {
+    const validPattern = SaveHeader.validCheckPattern;
+    return this.checkPattern.every((value, index) =>
+      value === validPattern[index]
+    );
+  }
+
+  private get isPaddingValid(): boolean {
+    const defaultPadding = SaveHeader.defaultPadding;
+    const debugPadding = SaveHeader.debugPadding;
+    return (
+      this.padding.every((value, index) => value === defaultPadding[index]) ||
+      this.padding.every((value, index) => value === debugPadding[index])
+    );
+  }
+
+  get isValid(): boolean {
+    return this.isPatternValid && this.isPaddingValid;
   }
 }
