ed870e88037ee80074fde38c0124efff13f82037
Conrad Calmez <conrad@calmez.net>
2025-02-27 07:45:49 +0100

    formatting

--- ui/desktop.ts
+++ ui/desktop.ts
@@ -3,6 +3,9 @@ import { SaveFile } from "../models/savefile.ts";
 import { Scene, Time } from "../models/scene.ts";
 import { LanguageOption, SoundOption } from "../models/saveheader.ts";
 
+console.log("Hello from desktop!");
+console.log(`In directory: ${Deno.cwd()}`);
+
 const webview = new Webview();
 webview.title = "oot-save-edit";
 
@@ -20,7 +23,9 @@ async function buildOutput(filename: string): Promise<string> {
       <p>Link has age: ${save.slots[0].age}</p>
       <p>Link has ${save.slots[0].rupees} rupees</p>
       <p>Link is actually called "${save.slots[0].playerName}"</p>
-      <p>Link's health is ${save.slots[0].currentHealth / 16}/${save.slots[0].maxHealth / 16}</p>
+      <p>Link's health is ${save.slots[0].currentHealth / 16}/${
+    save.slots[0].maxHealth / 16
+  }</p>
       <p>Link is in ${Scene[save.slots[0].savedSceneIndex]} scene</p>
       <p>The current time is ${Time[save.slots[0].nightFlag]}</p>
       <p>The current cutscene number is ${save.slots[0].cutSceneNumber}</p>
@@ -29,5 +34,9 @@ async function buildOutput(filename: string): Promise<string> {
   `;
 }
 
-webview.navigate(`data:text/html,${encodeURIComponent(await buildOutput('./test-data/new-game.srm'))}`);
+webview.navigate(
+  `data:text/html,${
+    encodeURIComponent(await buildOutput("test-data/new-game.srm"))
+  }`,
+);
 webview.run();
