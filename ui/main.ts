04498545cea4beb21ef2e2f89ce7251b866b4785
Conrad Calmez <conrad@calmez.net>
2025-03-01 16:54:14 +0100

    reworked ui into webview and server

--- ui/main.ts
+++ ui/main.ts
@@ -1,7 +1,10 @@
 import { Webview } from "@webview/webview";
-import { SaveFile } from "../models/savefile.ts";
-import { Scene, Time } from "../models/scene.ts";
-import { LanguageOption, SoundOption } from "../models/saveheader.ts";
+import { dirname, join } from "@std/path";
+
+const worker = new Worker(
+  join(dirname(import.meta.url), "server.ts"),
+  { type: "module" },
+);
 
 const webview = new Webview(true);
 webview.title = "oot-save-edit";
@@ -11,63 +14,8 @@ webview.bind("exit", () => {
   Deno.exit();
 });
 
-webview.bind("fileOpen", (filename: string) => {
-  webview.navigate(
-    `data:text/html,${
-      encodeURIComponent(
-        buildOutput(
-          // hack because we get a weird path from the file input
-          // e.g. C:\fakepath\oot.srm
-          `THIS_NEEDS_A_FIXED_PATH_TO_A_FOLDER_AS_A_WORKAROND/${filename.split('\\')[2]}`
-        )
-      )
-    }`,
-  );
-});
-
-function buildOutput(filename: string): string {
-  const file = Deno.openSync(filename);
-  const save = new SaveFile(file);
-  file.close();
-
-  return `
-    <html>
-    <body>
-      <h1>File: ${filename}</h1>
-      <p>Sound option: ${SoundOption[save.header.soundOption]}</p>
-      <p>Language option: ${LanguageOption[save.header.languageOption]}</p>
-      <p>Link has age: ${save.slots[0].age}</p>
-      <p>Link has ${save.slots[0].rupees} rupees</p>
-      <p>Link is actually called "${save.slots[0].playerName}"</p>
-      <p>Link's health is ${save.slots[0].currentHealth / 16}/${
-    save.slots[0].maxHealth / 16
-  }</p>
-      <p>Link is in ${Scene[save.slots[0].savedSceneIndex]} scene</p>
-      <p>The current time is ${Time[save.slots[0].nightFlag]}</p>
-      <p>The current cutscene number is ${save.slots[0].cutSceneNumber}</p>
-    </body>
-    </html>
-  `;
-}
-
-webview.navigate(
-  `data:text/html,${
-    encodeURIComponent(`
-      <html>
-        <body>
-          <h1>Open a file</h1>
-          <input id="file" type="file" accept=".srm" />
-          <button onclick="exit()">Exit</button>
-          <script>
-            const fileInput = document.getElementById("file");
-            fileInput.addEventListener("change", (event) => {
-              fileOpen(fileInput.value);
-            });
-          </script>
-        </body>
-      </html>
-    `)
-  }`,
-);
+webview.navigate(`http://127.0.0.1:8000`);
 
 webview.run();
+worker.terminate();
+Deno.exit();
---
+++ ui/server.ts
@@ -0,0 +1,75 @@
+import { join } from "@std/path";
+import { Application } from "jsr:@oak/oak/application";
+import { Router } from "jsr:@oak/oak/router";
+import { SaveFile } from "../models/savefile.ts";
+import { Scene, Time } from "../models/scene.ts";
+import { LanguageOption, SoundOption } from "../models/saveheader.ts";
+
+function buildOutput(filename?: string): string {
+  if (!filename) {
+    throw new Error("No filename provided");
+  }
+  const basePath = join(Deno.cwd(), "test-data"); // TODO: let user set this in UI
+  const file = Deno.openSync(`${basePath}/${filename}`);
+  const save = new SaveFile(file);
+  file.close();
+
+  return `
+    <html>
+    <body>
+      <h1>File: ${filename}</h1>
+      <p>Sound option: ${SoundOption[save.header.soundOption]}</p>
+      <p>Language option: ${LanguageOption[save.header.languageOption]}</p>
+      <p>Link has age: ${save.slots[0].age}</p>
+      <p>Link has ${save.slots[0].rupees} rupees</p>
+      <p>Link is actually called "${save.slots[0].playerName}"</p>
+      <p>Link's health is ${save.slots[0].currentHealth / 16}/${
+    save.slots[0].maxHealth / 16
+  }</p>
+      <p>Link is in ${Scene[save.slots[0].savedSceneIndex]} scene</p>
+      <p>The current time is ${Time[save.slots[0].nightFlag]}</p>
+      <p>The current cutscene number is ${save.slots[0].cutSceneNumber}</p>
+    </body>
+    </html>
+  `;
+}
+
+export default function runServer(controller?: AbortController) {
+  const router = new Router();
+  router.get("/", (ctx) => {
+    ctx.response.body = `
+      <html>
+        <body>
+          <h1>Open a file</h1>
+          <input id="file" type="file" accept=".srm" />
+          <button onclick="exit()">Exit</button>
+          <script>
+            // hack because we get a weird path from the file input
+            // e.g. C:\fakepath\oot.srm
+            // TODO let user set the path from the UI
+            const fileInput = document.getElementById("file");
+            fileInput.addEventListener("change", () => {
+              window.location = "/file/" + fileInput.value.split("\\\\")[2];
+            });
+          </script>
+        </body>
+      </html>
+    `;
+  });
+  router.get("/file/:filename", (ctx) => {
+    ctx.response.body = buildOutput(ctx.params.filename);
+  });
+
+  const app = new Application();
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+
+  app.listen({
+    port: 8000,
+    signal: controller?.signal,
+  });
+}
+
+if (import.meta.main) {
+  runServer();
+}
