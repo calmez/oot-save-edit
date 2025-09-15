583ed8a44da6a3724913ff43a87b37dfabd2a531
Conrad Calmez <conrad@calmez.net>
2025-09-15 23:16:02 +0200

    dipping toes lightly into telemetry

---
+++ utils/telemetry.ts
@@ -0,0 +1,45 @@
+import { metrics, trace, ValueType } from "npm:@opentelemetry/api@1";
+
+const appName = "oot-save-edit";
+
+const tracer = trace.getTracer(appName, "dev");
+const meter = metrics.getMeter(appName, "dev");
+
+export const Span = {
+  loadFile: (path: string) => {
+    return tracer.startSpan("load_file", {
+      attributes: {
+        "file.cwd": Deno.cwd(),
+        "file.path": path,
+      },
+    });
+  },
+  saveFile: (path: string, format: number, forceSwap: boolean) =>
+    tracer.startSpan("save_file", {
+      attributes: {
+        "file.cwd": Deno.cwd(),
+        "file.path": path,
+        "file.format": format,
+        "file.forceSwap": forceSwap,
+      },
+    }),
+};
+
+export const Metric = {
+  loadFileDuration: meter.createGauge(
+    `${appName}.load_file.duration`,
+    {
+      description: "Duration of loading a save file",
+      unit: "milliseconds",
+      valueType: ValueType.DOUBLE,
+    },
+  ),
+  saveFileDuration: meter.createGauge(
+    `${appName}.save_file.duration`,
+    {
+      description: "Duration of saving a save file",
+      unit: "milliseconds",
+      valueType: ValueType.DOUBLE,
+    },
+  ),
+};
