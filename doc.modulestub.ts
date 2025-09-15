bb866767cfd32b78a9190bbc7cc77c0186ec28a4
Conrad Calmez <conrad@calmez.net>
2025-09-15 23:54:55 +0200

    extended modulestub to make docs build work

--- doc.modulestub.ts
+++ doc.modulestub.ts
@@ -1,1 +1,24 @@
 export function assert(_condition: boolean, _msg?: string): void {}
+
+export const metrics = {
+  getMeter: (_name: string, _version?: string) => ({
+    createGauge: (_name: string, _options?: unknown) => ({
+      record: (_value: number) => {
+      },
+    }),
+  }),
+};
+export const trace = {
+  getTracer: (_name: string, _version?: string) => ({
+    startSpan: (_name: string, _options?: unknown) => ({
+      end: () => {
+      },
+      setAttributes: (_attributes: Record<string, unknown>) => {
+      },
+    }),
+  }),
+};
+export enum ValueType {
+  INT,
+  DOUBLE,
+}
