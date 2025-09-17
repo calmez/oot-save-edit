4d7c8f38eac0ce3ae6731a8f05fd324786452dbe
Conrad Calmez <conrad@calmez.net>
2025-09-17 18:28:45 +0200

    made slots more interactive by clicking to expand

--- ui/web/components/Save.tsx
+++ ui/web/components/Save.tsx
@@ -1,7 +1,7 @@
 import { SaveSlot } from "../../../models/saveslot.ts";
-import { Entrance, Room } from "../../../models/scene.ts";
 import { FileUtil } from "../../../utils/fileutil.ts";
 import { DownloadButton } from "../islands/DownloadButton.tsx";
+import Slot from "../islands/Slot.tsx";
 
 interface SaveProps {
   filename: string;
@@ -26,40 +26,7 @@ export default function Save(props: SaveProps) {
         </div>
         <div className="space-y-6">
           {saveFile.slots.map((slot: SaveSlot, idx: number) => (
-            <div key={idx} className="border rounded p-4 bg-gray-50">
-              <h3 className="text-xl font-semibold mb-2">Slot {idx + 1}</h3>
-              <div className="grid grid-cols-2 gap-4">
-                <div>
-                  <span className="font-medium">Player Name:</span>{" "}
-                  <span className="tabular-nums">{slot.playerName}</span>
-                </div>
-                <div>
-                  <span className="font-medium">Health:</span>{" "}
-                  <span className="tabular-nums">
-                    {slot.currentHealth / 16} / {slot.maxHealth / 16}{" "}
-                    ({slot.doubleDefenseHearts})
-                  </span>
-                </div>
-                <div>
-                  <span className="font-medium">Rupees:</span>{" "}
-                  <span className="tabular-nums">{slot.rupees}</span>
-                </div>
-                <div>
-                  <span className="font-medium">Location:</span>{" "}
-                  <span>{Room[slot.room]} ({Entrance[slot.entrance]})</span>
-                </div>
-                <div>
-                  <span className="font-medium">Deaths:</span>{" "}
-                  <span className="tabular-nums">{slot.deathCounter}</span>
-                </div>
-                <div>
-                  <span className="font-medium">Gold Skulltulas:</span>{" "}
-                  <span className="tabular-nums">
-                    {slot.goldSkulltulaTokens}
-                  </span>
-                </div>
-              </div>
-            </div>
+            <Slot slotData={slot.data} index={idx} />
           ))}
         </div>
       </div>
---
+++ ui/web/islands/Slot.tsx
@@ -0,0 +1,59 @@
+
+import { useState } from "preact/hooks";
+import { SaveSlot } from "../../../models/saveslot.ts";
+import { Entrance, Room } from "../../../models/scene.ts";
+
+interface SlotProps {
+  slotData: Uint8Array;
+  index: number;
+}
+
+export default function Slot(props: SlotProps) {
+  const { slotData, index } = props;
+  const slot = new SaveSlot(slotData);
+
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div key={index} className={`border rounded p-4 ${expanded ? "bg-blue-300/90 shadow-lg h-max" : "bg-gray-50 h-min"} hover:cursor-pointer`} onClick={() => {setExpanded(!expanded)}}>
+      <h3 className="text-xl font-semibold mb-2">File {index + 1}</h3>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <span className="font-medium">Player Name:</span>{" "}
+          <span className="tabular-nums">{slot.playerName}</span>
+        </div>
+        <div>
+          <span className="font-medium">Health:</span>{" "}
+          <span className="tabular-nums">
+            {slot.currentHealth / 16} / {slot.maxHealth / 16}{" "}
+            ({slot.doubleDefenseHearts / 16})
+          </span>
+        </div>
+        <div>
+          <span className="font-medium">Deaths:</span>{" "}
+          <span className="tabular-nums">{slot.deathCounter}</span>
+        </div>
+      </div>
+      {expanded && (
+        <div className="grid grid-cols-3 gap-4">
+          <div>
+            <span className="font-medium">Rupees:</span>{" "}
+            <span className="tabular-nums">{slot.rupees}</span>
+          </div>
+          <div>
+            <span className="font-medium">Magic Meter:</span>{" "}
+            <span className="tabular-nums">
+              {slot.currentMagic} / {slot.maxMagic}
+            </span>
+          </div>
+          <div>
+            <span className="font-medium">Location:</span>{" "}
+            <span className="tabular-nums">
+              {Room[slot.room]} ({Entrance[slot.entrance]})
+            </span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
