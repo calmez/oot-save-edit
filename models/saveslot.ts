be5f2c1be4d7102d4ccee9ee1f32dadb8e460215
Conrad Calmez <conrad@calmez.net>
2026-02-22 03:43:08 +0100

    formatting

--- models/saveslot.ts
+++ models/saveslot.ts
@@ -916,7 +916,9 @@ export class SaveSlot {
   private set permanentSceneFlags(value: Array<PermanentSceneFlags>) {
     if (value.length != (101)) {
       throw Error(
-        `permanent scene flags data needs to have ${101} scene entries (${101 * 0x1C} bytes), got ${value.length} entries.`,
+        `permanent scene flags data needs to have ${101} scene entries (${
+          101 * 0x1C
+        } bytes), got ${value.length} entries.`,
       );
     }
 
