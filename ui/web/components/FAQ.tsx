91702acd01d97be84869b5d7df1950a2f87dfa2f
Conrad Calmez <conrad@calmez.net>
2026-03-08 19:39:24 +0100

    reformat

--- ui/web/components/FAQ.tsx
+++ ui/web/components/FAQ.tsx
@@ -8,40 +8,50 @@ interface FAQItem {
 const data: FAQItem[] = [
   {
     question: "What is the difference between SRM and SRA save files?",
-    answer: <p>
-      SRM files are usually used by emulators like RetroArch. They
-      include different save file formats in one compact file. SRA on the other
-      hand is a format that just represents SRAM on an actual cartridge and is not
-      only used by original cartridges, but also Everdrives.
-    </p>,
+    answer: (
+      <p>
+        SRM files are usually used by emulators like RetroArch. They include
+        different save file formats in one compact file. SRA on the other hand
+        is a format that just represents SRAM on an actual cartridge and is not
+        only used by original cartridges, but also Everdrives.
+      </p>
+    ),
   },
   {
     question: "Can I use this tool on mobile devices?",
-    answer: <p>
-      The web interface should work on mobile devices. You can also use bookmark
-      functionality (i.e. on iOS) to keep the web application on your home
-      screen.
-    </p>
+    answer: (
+      <p>
+        The web interface should work on mobile devices. You can also use
+        bookmark functionality (i.e. on iOS) to keep the web application on your
+        home screen.
+      </p>
+    ),
   },
   {
     question: "How do I contribute to this project?",
-    answer: <p>
-      Contributions are welcome! You can contribute by reporting issues,
-      suggesting improvements, or submitting pull requests on our <a
-        href="https://github.com/calmez/oot-save-edit"
-        className="text-gray-900 underline"
-        target="_blank"
-        rel="noopener noreferrer">
+    answer: (
+      <p>
+        Contributions are welcome! You can contribute by reporting issues,
+        suggesting improvements, or submitting pull requests on our{" "}
+        <a
+          href="https://github.com/calmez/oot-save-edit"
+          className="text-gray-900 underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           GitHub repository
-      </a>.
-    </p>,
-  }
+        </a>.
+      </p>
+    ),
+  },
 ];
 
 export function FAQ() {
   return (
     <div className="flex flex-col gap-2 pt-8">
-      <h2 className="text-2xl font-bold mb-4 text-center">Frequently Asked Questions</h2>
+      <h2 className="text-2xl font-bold mb-4 text-center">
+        Frequently Asked Questions
+      </h2>
       {data.map((item, index) => (
         <details key={index} className="space-y-4">
           <summary className="cursor-pointer text-lg font-semibold text-slate-900">
